92738e4e066e9b868e0fe19b3ce6571274d84945
Mark Modak <mmodak@mmodak.local>
2018-11-10 17:10:38 -0700

    Fewer @instances and a stronger .cranky?

--- lib/centaur.rb
+++ lib/centaur.rb
@@ -1,6 +1,6 @@
 class Centaur
 
-  attr_reader :name, :breed
+  attr_reader :name, :breed, :laying, :cranky, :run_shoot_count, :standing, :rested, :sick
   def initialize(name, breed)
     @name = name
     @breed = breed
@@ -13,27 +13,27 @@ class Centaur
   end
 
   def shoot
-    return "NO!" if @laying || @cranky || @run_shoot_count >= 3
+    return "NO!" if cranky? || laying
     @run_shoot_count += 1
-    @cranky = true if @run_shoot_count >= 3
+    @cranky = true if run_shoot_count >= 3
     return "Twang!!!"
   end
 
   def run
-    return "NO!" if @run_shoot_count >= 3 || @cranky || @laying
+    return "NO!" if cranky? || laying
     @run_shoot_count += 1
-    @cranky = true if @run_shoot_count >= 3
+    @cranky = true if run_shoot_count >= 3
     return "Clop clop clop clop!!!"
   end
 
   def potion
-    return "NO!" unless @standing
-    @sick = true if @rested
+    return "NO!" unless standing
+    @sick = true if rested
     @rested = true
   end
 
   def sleep
-    return "NO!" if @standing
+    return "NO!" if standing
     @run_shoot_count = 0
     @cranky = false
   end
@@ -49,22 +49,22 @@ class Centaur
   end
 
   def standing?
-    @standing
+    standing
   end
 
   def laying?
-    @laying
+    laying
   end
 
   def rested?
-    @rested
+    rested
   end
 
   def sick?
-    @sick
+    sick
   end
 
   def cranky?
-    @cranky
+    cranky || run_shoot_count >= 3
   end
 end
